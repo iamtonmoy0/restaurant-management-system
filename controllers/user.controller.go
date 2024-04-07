ab3c4d4d9fac4ec15ae93a54a92f478af5a44038
iamtonmoy0 <tonmoy2code@gmail.com>
2024-04-07 23:31:06 +0600

    Add user handling functions and password hashing functions to user controller
    The changes include:
    
    * Importing the gin package for creating HTTP handlers
    * Defining new functions for handling user-related requests, including getting users, getting a single user, signing up, and logging in
    * Defining functions for hashing and verifying passwords

--- controllers/user.controller.go
+++ controllers/user.controller.go
@@ -1,2 +1,33 @@
 package controllers
 
+import "github.com/gin-gonic/gin"
+
+// get users
+func GetUsers() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+// get single user
+func GetUser() gin.HandlerFunc {
+	return func(c *gin.Context) {}
+}
+
+// sign up
+func SignUp() gin.HandlerFunc {
+	return func(c *gin.Context) {}
+}
+
+// login
+func Login() gin.HandlerFunc {
+	return func(c *gin.Context) {}
+}
+
+// hash password
+func HashPassword(password string) string {
+	return ""
+}
+
+// verify password
+func VerifyPassword(userPassword string, previousPassword string) (bool, string) { return "" }
