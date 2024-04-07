ae99f7945b57f0e13338c47ce6a7fa3107e030b2
iamtonmoy0 <tonmoy2code@gmail.com>
2024-04-08 00:18:13 +0600

    Add skeleton for CRUD handlers of Invoice controller using Gin framework
    The diff adds empty skeleton handlers for GetInvoices, GetInvoice, CreateInvoice, and UpdateInvoice functions in the Invoice controller using the Gin framework.

--- controllers/invoice.controller.go
+++ controllers/invoice.controller.go
@@ -1,2 +1,23 @@
 package controllers
 
+import "github.com/gin-gonic/gin"
+
+// get all invoice
+func GetInvoices() gin.HandlerFunc {
+	return func(c *gin.Context) {}
+}
+
+// get invoice
+func GetInvoice() gin.HandlerFunc {
+	return func(c *gin.Context) {}
+}
+
+// create new invoice
+func CreateInvoice() gin.HandlerFunc {
+	return func(c *gin.Context) {}
+}
+
+// update existing invoice
+func UpdateInvoice() gin.HandlerFunc {
+	return func(c *gin.Context) {}
+}
