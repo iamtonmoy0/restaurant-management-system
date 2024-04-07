f499e4652f64832e978daa9d265fb5dbda335eb9
iamtonmoy0 <tonmoy2code@gmail.com>
2024-04-07 21:27:44 +0600

    Add Gin framework and set up basic server
    The diff adds the Gin web framework to the project and sets up a basic server in the `main` function. The server listens on a port specified by the "PORT" environment variable, or defaults to 300

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"os"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "3000"
+	}
+	r := gin.New()
+	r.Use(gin.Logger())
+
+	r.Run(":" + port)
 }
