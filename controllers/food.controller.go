7f8dca49b941936d49972b09b87654256a878bb6
iamtonmoy0 <tonmoy2code@gmail.com>
2024-04-08 00:12:20 +0600

    "Add handlers for CRUD operations in food controller"
    This commit adds new functions to the food controller for handling CRUD operations. The functions include:
    
    * GetFoods: Handler for getting all foods
    * CreateFood: Handler for creating a new food
    * UpdateFood: Handler for updating an existing food
    
    Additionally, a new function `round` is added for rounding a floating-point number to a specified number of decimal places. However, this function is not used in any of the handlers.

--- controllers/food.controller.go
+++ controllers/food.controller.go
@@ -1,2 +1,23 @@
 package controllers
 
+import "github.com/gin-gonic/gin"
+
+// get all foods
+func GetFoods() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+// create food
+func CreateFood() gin.HandlerFunc {
+	return func(c *gin.Context) {}
+}
+
+// update food
+func UpdateFood() gin.HandlerFunc {
+	return func(c *gin.Context) {}
+}
+func round(num float64, precision int) float64 {
+
+}
