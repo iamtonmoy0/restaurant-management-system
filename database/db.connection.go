5a261deba8b5094d5a810160a7457a4a8bd00d84
iamtonmoy0 <tonmoy2code@gmail.com>
2024-04-07 22:06:40 +0600

    "Added database connection using Mongodb, godotenv and updated go.mod/go.sum"
    This commit includes:
    - Added a new function `DBinstance()` in the `database/db.connection.go` file to connect to MongoDB using the URI from the `.env` file.
    - Added import of required packages in `database/db.connection.go`.
    - Added `godotenv` package in `go.mod/go.sum` and updated the dependencies.
    - Commented out the unused routes in `routes/user.router.go`.

--- database/db.connection.go
+++ database/db.connection.go
@@ -1,1 +1,40 @@
 package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBinstance() *mongo.Client {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	mongoDB := os.Getenv("URI")
+	client, err := mongo.NewClient(options.Client().ApplyURI(mongoDB))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("successfully connected to  MongoDB server")
+	return client
+}
+
+var Client *mongo.Client = DBinstance()
+
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database("Restaurant_Management").Collection(collectionName)
+	return collection
+}
