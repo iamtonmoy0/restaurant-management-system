ccfe08aaccaac3652048e9c93b94e145e93da083
iamtonmoy0 <tonmoy2code@gmail.com>
2024-04-10 03:57:56 +0600

    "Updated GetMenus function to retrieve all menu items from MongoDB collection"
    This commit modifies the GetMenus function in the menu.controller.go file to fetch all menu items from a MongoDB database collection. It introduces context timeouts, error handling, and querying of the MongoDB collection to return the menu items as JSON.

--- controllers/menu.controller.go
+++ controllers/menu.controller.go
@@ -1,15 +1,43 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/iamtonmoy0/restaurant-management-system/database"
+	"go.mongodb.org/mongo-driver/mongo"
+	"gopkg.in/mgo.v2/bson"
+)
+
+// collections
+var menuCollection *mongo.Collection = database.OpenCollection(database.Client, "menu")
 
 // get all Menu
 func GetMenus() gin.HandlerFunc {
-	return func(c *gin.Context) {}
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		result, err := menuCollection.Find(ctx, bson.M{})
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		var allMenus []bson.M
+		if err := result.All(ctx, &allMenus); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, allMenus)
+	}
 }
 
 // get Menu
 func GetMenu() gin.HandlerFunc {
-	return func(c *gin.Context) {}
+	return func(c *gin.Context) {
+
+	}
 }
 
 // create new Menu
