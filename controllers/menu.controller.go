5b0bb9411da8f7948dd08a674ef1a221c5d613bc
iamtonmoy0 <tonmoy2code@gmail.com>
2024-04-10 23:05:27 +0600

    "Added models package and updated GetMenu function to find and return a single menu by ID"
    This commit updates the `GetMenu` function to find and return a single menu by its ID. It adds a new import statement for the `models` package and uses it to define a `menu` variable of type `models.Menu`. The function then finds the menu with the specified ID using the `FindOne` method and returns it in the response. If there is an error while finding the menu, the function responds with an HTTP 500 status code and an error message. Otherwise, it responds with an HTTP 200 status code and the menu data. The `context.WithTimeout` function is used to set a timeout for the database operation.

--- controllers/menu.controller.go
+++ controllers/menu.controller.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/iamtonmoy0/restaurant-management-system/database"
+	"github.com/iamtonmoy0/restaurant-management-system/models"
 	"go.mongodb.org/mongo-driver/mongo"
 	"gopkg.in/mgo.v2/bson"
 )
@@ -36,7 +37,18 @@ func GetMenus() gin.HandlerFunc {
 // get Menu
 func GetMenu() gin.HandlerFunc {
 	return func(c *gin.Context) {
-
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		menuId := c.Param("id")
+		var menu models.Menu
+		// find the data
+		err := menuCollection.FindOne(ctx, bson.M{"menu_id": menuId}).Decode(&menu)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error occurred while finding the data"})
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"data": menu,
+		})
 	}
 }
 
