516c761c0f478b51f2249685c6c8d84bee93d1d3
iamtonmoy0 <tonmoy2code@gmail.com>
2024-05-08 21:16:38 +0600

    "Add InvoiceViewFormat struct, invoiceCollection and GetInvoices function; initialize invoiceCollection with MongoDB collection 'invoice'"
    This commit adds a new struct called InvoiceViewFormat, which defines the format for

--- controllers/invoice.controller.go
+++ controllers/invoice.controller.go
@@ -1,7 +1,27 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"time"
 
+	"github.com/gin-gonic/gin"
+	"github.com/iamtonmoy0/restaurant-management-system/database"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type InvoiceViewFormat struct {
+	Invoice_id       string
+	Payment_method   string
+	Order_id         string
+	Payment_status   *string
+	Payment_due      interface{}
+	Table_number     interface{}
+	Payment_due_date time.Time
+	Order_details    interface{}
+}
+
+var invoiceCollection *mongo.Collection = database.OpenCollection(database.Client, "invoice")
+
+// TODO: need to work on invoice controller 
 // get all invoice
 func GetInvoices() gin.HandlerFunc {
 	return func(c *gin.Context) {}
