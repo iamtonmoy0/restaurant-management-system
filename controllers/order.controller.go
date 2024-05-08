a837874380bd0e9ba2b8f68fec5df4715f6b3f64
iamtonmoy0 <tonmoy2code@gmail.com>
2024-05-08 15:28:33 +0600

    "Update GetOrders function to fetch all orders from MongoDB collection"
    This commit updates the GetOrders function to fetch all orders from the MongoDB order collection using the Go driver. It introduces context with timeout, uses

--- controllers/order.controller.go
+++ controllers/order.controller.go
@@ -1,10 +1,36 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/iamtonmoy0/restaurant-management-system/database"
+	"go.mongodb.org/mongo-driver/mongo"
+	"gopkg.in/mgo.v2/bson"
+)
+
+var orderCollection *mongo.Collection = database.OpenCollection(database.Client, "order")
 
 // get all Order
 func GetOrders() gin.HandlerFunc {
-	return func(c *gin.Context) {}
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+
+		result, err := orderCollection.Find(context.TODO(), bson.M{})
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
+		var allOrders []bson.M
+		if err = result.All(ctx, &allOrders); err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, allOrders)
+
+	}
 }
 
 // get Order
