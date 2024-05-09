24b7e0da991ef071b32d8a0625cdbb20d4a88f6b
iamtonmoy0 <tonmoy2code@gmail.com>
2024-05-09 18:37:16 +0600

    Update existing invoice: add functionality to update invoice fields in the database
    - Add context with timeout for database operations
    - Bind updated invoice data from JSON request body
    - Define filter for querying specific invoice by invoice

--- controllers/invoice.controller.go
+++ controllers/invoice.controller.go
@@ -12,6 +12,7 @@ import (
 	"github.com/iamtonmoy0/restaurant-management-system/models"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"gopkg.in/mgo.v2/bson"
 )
 
@@ -133,5 +134,58 @@ func CreateInvoice() gin.HandlerFunc {
 
 // update existing invoice
 func UpdateInvoice() gin.HandlerFunc {
-	return func(c *gin.Context) {}
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+		var invoice models.Invoice
+		invoiceId := c.Param("invoice_id")
+
+		if err := c.BindJSON(&invoice); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		filter := bson.M{"invoice_id": invoiceId}
+
+		var updateObj primitive.D
+
+		if invoice.Payment_method != nil {
+			updateObj = append(updateObj, bson.E{"payment_method", invoice.Payment_method})
+		}
+
+		if invoice.Payment_status != nil {
+			updateObj = append(updateObj, bson.E{"payment_status", invoice.Payment_status})
+		}
+
+		invoice.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		updateObj = append(updateObj, bson.E{"updated_at", invoice.Updated_at})
+
+		upsert := true
+		opt := options.UpdateOptions{
+			Upsert: &upsert,
+		}
+
+		status := "PENDING"
+		if invoice.Payment_status == nil {
+			invoice.Payment_status = &status
+		}
+
+		result, err := invoiceCollection.UpdateOne(
+			ctx,
+			filter,
+			bson.D{
+
+				{"$set", updateObj},
+			},
+			&opt,
+		)
+		if err != nil {
+			msg := fmt.Sprintf("invoice item update failed")
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+		}
+
+		defer cancel()
+		c.JSON(http.StatusOK, result)
+	}
 }
