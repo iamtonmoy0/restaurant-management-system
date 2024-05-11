1e95b7dca7484852c834008bbf632efd1e68f1e1
iamtonmoy0 <tonmoy2code@gmail.com>
2024-05-11 16:02:35 +0600

    "Update user controller with new functions and convert to use mongo driver"
    This commit updates the `user.controller.go` file to include new functions for getting users, getting a single user, signing up, and logging in. It

--- controllers/user.controller.go
+++ controllers/user.controller.go
@@ -1,27 +1,113 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/iamtonmoy0/restaurant-management-system/database"
+	"github.com/iamtonmoy0/restaurant-management-system/models"
+	"go.mongodb.org/mongo-driver/mongo"
+	"gopkg.in/mgo.v2/bson"
+)
+
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
 
 // get users
 func GetUsers() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		// record per page
+		recordPerPage, err := strconv.Atoi(c.Query("recordPerPage"))
+		if err != nil || recordPerPage < 1 {
+			recordPerPage = 10
+		}
+		// page
+		page, err := strconv.Atoi(c.Query("page"))
+		if err != nil || page < 1 {
+			page = 1
+		}
+		// start index/ skip data par page
+		startIndex := (page - 1) * recordPerPage
+		startIndex, err := strconv.Atoi(c.Query("startIndex"))
+
+		matchStage := bson.D{{"$match", bson.D{{}}}}
+		projectStage := bson.D{
+			{"$project", bson.D{
+				{"_id", 0},
+				{"total_count", 1},
+				{"user_items", bson.D{{"$slice", []interface{}{"$data", startIndex, recordPerPage}}}},
+			}}}
+		// query the data
+		result, err := userCollection.Aggregate(ctx, mongo.Pipeline{
+			matchStage, projectStage})
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occurred while listing user items"})
+		}
+
+		var allUsers []bson.M
+		if err = result.All(ctx, &allUsers); err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, allUsers[0])
 
 	}
 }
 
 // get single user
 func GetUser() gin.HandlerFunc {
-	return func(c *gin.Context) {}
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		userId := c.Param("user_id")
+
+		var user models.User
+
+		err := userCollection.FindOne(ctx, bson.M{"user_id": userId}).Decode(&user)
+
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while listing user items"})
+		}
+		c.JSON(http.StatusOK, user)
+	}
 }
 
 // sign up
 func SignUp() gin.HandlerFunc {
-	return func(c *gin.Context) {}
+	return func(c *gin.Context) {
+		// convert the data
+
+		// validate data based on user struct
+
+		// check if email already in use or not
+
+		// hash password
+
+		// create details like createdAt and Updated At
+
+		// generate token and refresh token
+
+		// insert data to db
+	}
 }
 
 // login
 func Login() gin.HandlerFunc {
-	return func(c *gin.Context) {}
+	return func(c *gin.Context) {
+		// convert json data to golang readable format
+
+		// find the user with email
+
+		// verify password
+
+		// update tokens
+
+		// return ok
+	}
 }
 
 // hash password
--- helpers/token.go
+++ helpers/token.go
@@ -1,1 +1,8 @@
 package helpers
+
+import (
+	"github.com/iamtonmoy0/restaurant-management-system/database"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var user *mongo.Collection = database.OpenCollection(database.Client, "user")
